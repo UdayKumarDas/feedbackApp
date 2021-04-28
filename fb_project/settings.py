6fc023229620f8e892642bf6454584bdc2d87546
UdayKumarDas <dasudaykumar017@gmail.com>
2021-04-28 17:42:03 +0100

    settings updated

--- fb_project/settings.py
+++ fb_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'fb_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'movie-api.cpgrne8qlv8b.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
