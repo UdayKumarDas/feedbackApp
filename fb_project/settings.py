7541946f0ba2d84075579e4d16ee410945dcd11a
UdayKumarDas <dasudaykumar017@gmail.com>
2021-04-29 09:06:53 +0100

    js and css files added

--- fb_project/settings.py
+++ fb_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import os.path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -124,8 +124,9 @@ USE_TZ = True
 
 #STATIC_URL = os.path.join(BASE_DIR, '/static/')
 #PROJECT_ROOT = "http://feedbackeb-env.eba-pi9iwmem.us-east-1.elasticbeanstalk.com"
-STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
+STATIC_ROOT =''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join('static'), )
 # import mimetypes
 # mimetypes.add_type("text/css", ".css", True)
